a1f7dcedd4d154f3063f01a4b512cf1703c295f8
Md Shahbaz Alam <141826020+itsshahbazhere@users.noreply.github.com>
2024-10-02 19:51:30 +0530

    Update Leet-Code-1518.cpp
    solving question LeetCode-1518

--- LeetCode-Consistency/Leet-Code-1518.cpp
+++ LeetCode-Consistency/Leet-Code-1518.cpp
@@ -1,2 +1,23 @@
 // 1518. Water Bottles
 // https://leetcode.com/problems/water-bottles/
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        
+        int emptyBottles = numBottles; // 15
+        int drinkBottles = numBottles; //15
+        int exchangeBottles = emptyBottles / numExchange; //3,3
+
+        while(emptyBottles >= numExchange){ //15>4 6>4 3>4
+
+            drinkBottles += exchangeBottles; //15+3=18+1=19
+            emptyBottles += exchangeBottles - exchangeBottles*numExchange; //15+3-12=6+1-4=3
+            exchangeBottles = emptyBottles / numExchange; //1,2 //0,3
+
+        }
+
+        return drinkBottles;
+        
+    }
+};
